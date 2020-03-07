d7ae87ebfe0be765d6f8fae59ad48803ff0ceda9
gauravm-bit <gauravwrk01@gmail.com>
2020-03-07 18:30:53 +0530

    ADDED:singlet combination using dict

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+declare -A dict
 
 echo "Welcome to flipCoin simulator "
-checkRandom=$((RANDOM%2))
-if [ $checkRandom -eq 0 ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi 
+
+num=15
+for(( i = 0; i < num; i++ ))
+do
+	checkRandom=$((RANDOM%2))
+	if [ $checkRandom -eq 0 ]
+	then
+		dict[heads]=$((${dict[heads]}+1))
+		Hpercentage=$((${dict[heads]}*100/num))
+	else
+		dict[tails]=$((${dict[tails]}+1))
+		Tpercentage=$((${dict[tails]}*100/num))
+	fi
+done
+
+echo  "tail percent is $Tpercentage" "head percent is $Hpercentage" 
+
+
