23ab71c8f055cde5ec9a625a93d6085d9530a4e1
gauravm-bit <gauravwrk01@gmail.com>
2020-03-08 09:17:47 -0700

    ADDED:doublet combination with percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
 
 declare -A dict
+declare -A doubleDict
 
 echo "Welcome to flipCoin simulator "
+read -p "Enter the number of times coin to be flipped " num
 
-num=15
+
 for(( i = 0; i < num; i++ ))
 do
 	checkRandom=$((RANDOM%2))
@@ -18,6 +20,40 @@ do
 	fi
 done
 
-echo  "tail percent is $Tpercentage" "head percent is $Hpercentage" 
+echo  "Singlet outcome is tail percent is $Tpercentage" "head percent is $Hpercentage" 
 
 
+for (( i = 0; i < num; i++ ))
+do
+   randomCheck1=$((RANDOM%2))
+   randomCheck2=$((RANDOM%2))
+   if (( $randomCheck1 == 0 && $randomCheck2 == 0 ))
+   then
+      doubleDict[HH]=$((${doubleDict[HH]}+1))
+   elif (( $randomCheck1 == 0 && $randomCheck2 == 1 ))
+   then
+		doubleDict[HT]=$((${doubleDict[HT]}+1))
+	elif (( $randomCheck1 == 1 && $randomCheck2 == 0 ))
+	then
+		doubleDict[TH]=$((${doubleDict[TH]}+1))
+	else
+		doubleDict[TT]=$((${doubleDict[TT]}+1))
+	fi
+done
+
+echo "doublet answer is"
+
+
+HHpercent=`echo "scale = 3 ; (${doubleDict[HH]}*100)/$num" | bc -l`
+THpercent=`echo "scale = 3 ; (${doubleDict[TH]}*100)/$num" | bc -l`
+HTpercent=`echo "scale = 3 ; (${doubleDict[HT]}*100)/$num" | bc -l`
+TTpercent=`echo "scale = 3 ; (${doubleDict[TT]}*100)/$num" | bc -l`
+
+echo "HH came "${doubleDict[HH]}
+echo "percentage of HH is " $HHpercent"%"
+echo "TH came "${doubleDict[TH]}
+echo "percentage of TH is " $THpercent"%"
+echo "HT came "${doubleDict[HT]}
+echo "percentage of HT is " $HTpercent"%"
+echo "TT came "${doubleDict[TT]}
+echo "percentage of TT is " $TTpercent"%"
